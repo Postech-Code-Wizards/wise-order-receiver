1b7faa958bd2f717ef73de0ac40e5f3448693f21
andy-spch <anderson.donaruma@gmail.com>
2025-07-19 18:31:08 -0300

    removed tests and configuring enum

--- src/main/java/br/com/wise/orderreceiver/domain/enums/PaymentMethodTypeEnum.java
+++ src/main/java/br/com/wise/orderreceiver/domain/enums/PaymentMethodTypeEnum.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 public enum PaymentMethodTypeEnum {
     CREDIT_CARD("CREDIT_CARD"),
     DEBIT_CARD("DEBIT_CARD");
-
     private final String value;
 
     PaymentMethodTypeEnum(String value) {
